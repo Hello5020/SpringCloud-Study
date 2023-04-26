7f12903bcd5d47cbbd884ec3ca4c6636c85db17d
Hello <2504788150@qq.com>
2023-04-26 14:34:29 +0800

    first push

---
+++ eureka-server/src/main/java/cn/ticast/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.ticast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @className: EurekaApplication
+ * @author: crowgzy
+ * @date: 2023/4/26
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
